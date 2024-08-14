dfed6cf4b1a0484f9fb46de1ab977e256f8a8624
Luis Rengifo <luis.rengifo402@gmail.com>
2024-08-14 08:29:59 -0500

    Commit DOCKERHUB_TOKEN changes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,7 +6,7 @@ const start = async () => {
   console.log('Starting up once again to test deploy!!!... another voice!');
   console.log('Maybe now??');
   console.log('Switch to trunk-based development!');
-  console.log('Trying use of DOCKERHUB_TOKEN!!');
+  console.log('Trying use of DOCKERHUB_TOKEN!!  ');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
