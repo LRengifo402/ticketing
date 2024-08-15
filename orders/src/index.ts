cd38a4d63f4c32c95cc8bc409ec09c87e4c90189
Luis Rengifo <luis.rengifo402@gmail.com>
2024-08-15 05:41:46 -0500

    Added Startup Messages to all Services.

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,9 +8,7 @@ import { PaymentCreatedListener } from './events/listeners/payment-created-liste
 import { Listener } from '@larengifo-ticks/common';
 
 const start = async () => {
-  console.log(
-    'Starting once again dude!!! ... Testing selective testing on github...!!!@2'
-  );
+  console.log('Starting Orders Service ...');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
